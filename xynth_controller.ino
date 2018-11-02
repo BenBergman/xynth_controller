ec3a5fb53d2f597982c2ae5d30419a933e6d7675
Ben Bergman <ben@benbergman.ca>
2018-11-02 17:39:01 -0500

    Add keyboard output

--- xynth_controller.ino
+++ xynth_controller.ino
@@ -5,6 +5,16 @@
  */
 
 #include <Encoder.h>
+#include <Keyboard.h>
+
+#define NUM_1 225
+#define NUM_2 226
+#define NUM_3 227
+#define NUM_4 228
+#define NUM_5 229
+#define NUM_6 230
+#define NUM_7 231
+#define NUM_8 232
 
 // Change these two numbers to the pins connected to your encoder.
 //   Best Performance: both pins have interrupt capability
@@ -31,6 +41,8 @@ void setup() {
 
   pinMode(knobButtonPin, INPUT_PULLUP);
   pinMode(mainButtonPin, INPUT_PULLUP);
+
+  Keyboard.begin();
 }
 
 long oldPosition  = -999;
@@ -38,6 +50,17 @@ long oldPosition  = -999;
 void loop() {
   long newPosition = myEnc.read();
   if (newPosition != oldPosition) {
+    if (newPosition > oldPosition || oldPosition - newPosition > 2000000) {
+      for (int i = oldPosition; i < newPosition; i++) {
+        Keyboard.press(NUM_1);
+        Keyboard.release(NUM_1);
+      }
+    } else {
+      for (int i = newPosition; i < oldPosition; i++) {
+        Keyboard.press(NUM_2);
+        Keyboard.release(NUM_2);
+      }
+    }
     oldPosition = newPosition;
     Serial.print("New encoder position: ");
     Serial.println(newPosition);
@@ -52,6 +75,11 @@ void loop() {
   if ((millis() - lastKnobDebounceTime) > debounceDelay) {
     if (knobButtonReading != knobButtonState) {
       knobButtonState = knobButtonReading;
+      if (knobButtonState == LOW) {
+        Keyboard.press(NUM_3);
+      } else {
+        Keyboard.release(NUM_3);
+      }
       Serial.print("debounced knob button is now ");
       Serial.println(knobButtonState);
     }
@@ -67,6 +95,11 @@ void loop() {
   if ((millis() - lastMainDebounceTime) > debounceDelay) {
     if (mainButtonReading != mainButtonState) {
       mainButtonState = mainButtonReading;
+      if (mainButtonState == LOW) {
+        Keyboard.press(NUM_4);
+      } else {
+        Keyboard.release(NUM_4);
+      }
       Serial.print("debounced main button is now ");
       Serial.println(mainButtonState);
     }
