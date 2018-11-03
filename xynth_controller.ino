18cf5d5e92251ca35049b4a2e3292debb9173016
Ben Bergman <ben@benbergman.ca>
2018-11-03 15:09:41 -0500

    Switch from keyboard emulation to gamepad emulation

--- xynth_controller.ino
+++ xynth_controller.ino
@@ -5,7 +5,9 @@
  */
 
 #include <Encoder.h>
-#include <Keyboard.h>
+#include <Joystick.h>
+
+Joystick_ Joystick;
 
 #define NUM_1 225
 #define NUM_2 226
@@ -35,6 +37,18 @@ unsigned long lastMainDebounceTime = 0;
 
 unsigned long debounceDelay = 50;
 
+
+
+
+
+float mapFloat(float x, float in_min, float in_max, float out_min, float out_max)
+{
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+
+
+
 void setup() {
   Serial.begin(9600);
   Serial.println("Basic Encoder Test:");
@@ -42,7 +56,9 @@ void setup() {
   pinMode(knobButtonPin, INPUT_PULLUP);
   pinMode(mainButtonPin, INPUT_PULLUP);
 
-  Keyboard.begin();
+  Joystick.setXAxisRange(-32768, 32767);
+  Joystick.setYAxisRange(-32768, 32767);
+  Joystick.begin();
 }
 
 long oldPosition  = -999;
@@ -50,17 +66,46 @@ long oldPosition  = -999;
 void loop() {
   long newPosition = myEnc.read();
   if (newPosition != oldPosition) {
-    if (newPosition > oldPosition || oldPosition - newPosition > 2000000) {
-      for (int i = oldPosition; i < newPosition; i++) {
-        Keyboard.press(NUM_1);
-        Keyboard.release(NUM_1);
-      }
+    int rotation = newPosition % 80;
+
+    // Map axis to follow a perfect circle from -1 to 1
+    float xAxis = sin(rotation * 2.0 * PI / 80.0);
+    float yAxis = -cos(rotation * 2.0 * PI / 80.0);
+
+    // May axis to follow a perfect square from -1 to 1
+    if (rotation < 10) {
+      xAxis = mapFloat(rotation * 1.0, 0.0, 10.0, 0.0, 1.0);
+      yAxis = -1.0;
+    } else if (rotation < 30) {
+      xAxis = 1.0;
+      yAxis = mapFloat(rotation * 1.0, 10.0, 30.0, -1.0, 1.0);
+    } else if (rotation < 50) {
+      xAxis = mapFloat(rotation * 1.0, 30.0, 50.0, 1.0, -1.0);
+      yAxis = 1.0;
+    } else if (rotation < 70) {
+      xAxis = -1.0;
+      yAxis = mapFloat(rotation * 1.0, 50.0, 70.0, 1.0, -1.0);
     } else {
-      for (int i = newPosition; i < oldPosition; i++) {
-        Keyboard.press(NUM_2);
-        Keyboard.release(NUM_2);
-      }
+      xAxis = mapFloat(rotation * 1.0, 70.0, 80.0, -1.0, 0.0);
+      yAxis = -1.0;
     }
+
+    // Map axis to fill the axis range
+    int16_t xMapped = xAxis * 32767.0;
+    int16_t yMapped = yAxis * 32767.0;
+
+    Joystick.setXAxis(xMapped);
+    Joystick.setYAxis(yMapped);
+
+    Serial.print("X axis: ");
+    Serial.print(xAxis);
+    Serial.print("   ");
+    Serial.println(xMapped);
+    Serial.print("Y axis: ");
+    Serial.print(yAxis);
+    Serial.print("   ");
+    Serial.println(yMapped);
+
     oldPosition = newPosition;
     Serial.print("New encoder position: ");
     Serial.println(newPosition);
@@ -76,9 +121,9 @@ void loop() {
     if (knobButtonReading != knobButtonState) {
       knobButtonState = knobButtonReading;
       if (knobButtonState == LOW) {
-        Keyboard.press(NUM_3);
+        Joystick.pressButton(1);
       } else {
-        Keyboard.release(NUM_3);
+        Joystick.releaseButton(1);
       }
       Serial.print("debounced knob button is now ");
       Serial.println(knobButtonState);
@@ -96,9 +141,9 @@ void loop() {
     if (mainButtonReading != mainButtonState) {
       mainButtonState = mainButtonReading;
       if (mainButtonState == LOW) {
-        Keyboard.press(NUM_4);
+        Joystick.pressButton(0);
       } else {
-        Keyboard.release(NUM_4);
+        Joystick.releaseButton(0);
       }
       Serial.print("debounced main button is now ");
       Serial.println(mainButtonState);
